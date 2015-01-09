10e6e61d0d00c7851d366dfdfed5265fb4716030
Stephen Cook <StephenTangCook@Gmail.com>
2015-01-08 21:05:42 -0500

    HTML panels draft

--- build/editor.js
+++ build/editor.js
@@ -1,1 +1,10 @@
 console.log("BITCOIN IDE! YEAH!");
+
+/* All javascript manipulation for the page goes in here: */
+$( document ).ready(function() {
+	/* All javascript manipulation for the page goes in here */
+
+	$("#stack-visualizer").text(editor.getSession().getValue());
+
+
+});
