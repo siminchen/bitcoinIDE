553b5b608b999afd1b51d0446cf11a7111408b87
Stephen Cook <Stephen@nat-oitwireless-inside-vapornet100-b-16181.princeton.edu>
2015-01-10 13:01:20 -0500

    Need to fix remove/insert timings

--- build/editor.js
+++ build/editor.js
@@ -6,19 +6,20 @@ var stackVisualizer;
 $( document ).ready(function() {
 	/* All javascript manipulation for the page goes in here */
 
-	//$("#stack-visualizer").text(editor.getSession().getValue());
+	//editor.getSession().getValue();
 
 	stackVisualizer = new StackVisualizer("stack-visualizer");
 	stackVisualizer.push("STACK VALUE TEST1");
 	stackVisualizer.push("STACK VALUE TEST2");
 	stackVisualizer.push("STACK VALUE TEST3");
-	p = stackVisualizer.pop();
-	stackVisualizer.pop();
-	stackVisualizer.push("STACK VALUE TEST4");
-	stackVisualizer.push("STACK VALUE TEST4.5");
-	p = stackVisualizer.remove(3);
-	stackVisualizer.insert("STACK VALUE TEST5", 2);
-	p = stackVisualizer.remove(3);
-	p = stackVisualizer.pop();
-	stackVisualizer.push("STACK VALUE TEST6");
+	// p = stackVisualizer.pop();
+	p = stackVisualizer.remove(2);
+	// // stackVisualizer.pop();
+	// stackVisualizer.push("STACK VALUE TEST4");
+	// stackVisualizer.push("STACK VALUE TEST4.5");
+	p = stackVisualizer.remove(2);
+	// stackVisualizer.insert("STACK VALUE TEST5", 2);
+	// p = stackVisualizer.remove(3);
+	// p = stackVisualizer.pop();
+	// stackVisualizer.push("STACK VALUE TEST6");
 });
