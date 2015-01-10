6cfbe65820d4efc38a53d89993033dc6fd0b4e41
Tom Wu <wtbneyc@hotmail.com>
2015-01-10 17:06:16 -0500

    Script debugger. Still need to combine it with the interpreter

--- build/debugger.js
+++ build/debugger.js
@@ -1,18 +1,65 @@
+// JS Module for the Bitcoin debugger
+// Usage: First instantiate ScriptDebugger.
+//        To run the script from beginning to the end, call runFromBeginning()
+//        To step to the next command, call nextStep()
+//        To continue execution till the end, call continueExecution() 
+
+function ScriptDebugger() {
+    this.pause = 1000; // Number of milliseconds of pause between commands
+    this.initialize();
+    this.visibleStack = new StackVisualizer("visible");
+    this.hiddenStack = new StackVisualizer("hidden");
+}
 
 
-var stepFromBeginning = function() {
-    // Set a global variable to indicate the index of the command array
-    env.index = 0;
-    nextStep();
+// This function is called when the class is instantiated,
+// when the function runFromBeginning is called, or when
+// the end of a script has been reached.
+ScriptDebugger.prototype.initialize = function() {
+    var script = editor.getSession().getValue();
+    // Split the script based on space characters
+    this.commands = script.split(/\s+/);
+    // this.visibleStack.clear();
+    // this.hiddenStack.clear();
+    this.index = 0; // The current index in the commands array to execute
+    this.needToInitialize = false;
+};
+
+ScriptDebugger.prototype.runFromBeginning = function() {
+    this.initialize();
+
+    while (this.index != -1 && this.index != -2) {
+	this.nextStep();
+    }
+
+    this.needToInitialize = true;
 };
 
 
-var nextStep = function() {
-    var inputRegion = document.querySelector(".ace_scoller");
-    var script = inputRegion.textContent;
+ScriptDebugger.prototype.nextStep = function(){
+    if (this.needToInitialize == true) {
+	this.initialize();
+    }  
+    //
+    if (this.index != 5) {
+	this.index += 1;
+    } else {
+	this.index = -1;
+    }
+    console.log(this.index);
+    //this.index = interpreter.nextStep(this.visibleStack, this.hiddenStack, this.commands, this.index);
+};
 
-    // Split the script based on space characters
-    var commands = script.split(/\s+/);
 
-    env.index = interpreter.nextStep(env.index);
+ScriptDebugger.prototype.continueExecution = function() {
+    if (this.needToInitialize == true) {
+	this.initialize();
+    }
+
+    while (this.index != -1 && this.index != -2) {
+	this.nextStep();
+    }
+
+    this.needToInitialize = true;
+
 };
